5c4bd41464344c2f0cdd98721de88ce827d3aa5f
Abimanyu H K <abimanyu.hk@cxtorg.net>
2019-05-28 13:13:13 +0530

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme:
+    description = readme.read()
+
+with open("LICENSE", "r") as readme:
+    license_x = readme.read()
+
 setup(
     name='json_utils',
     version='1.0.0',
-    packages=['json_utils.test'],
+    packages=['json_utils'],
     url='https://github.com/AbhimanyuHK/Json_Object_Conv',
-    license='mit',
-    author='abimanyu.hk',
+    license=license_x,
+    author='Abimanyu H K',
     author_email='manyu1994@hotmail.com',
-    description='json_utils'
+    description=description
 )
